f534c34e5301ff0d2f4121889818197cf23ac7d6
Nur Rahman <nurrahman.org@gmail.com>
2022-08-14 17:09:44 +0700

    Menambahkan 4 halaman web

--- system/routes/web.php
+++ system/routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+Route::get('/details', function () {
+    return view('details');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/products', function () {
+    return view('products');
 });
